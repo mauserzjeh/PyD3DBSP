f43155fb33245771ee6e0af8927b9410bddb6bec
Soma Rádóczi <mauserzjeh@gmail.com>
2019-04-26 23:15:28 +0200

    Basic add-on interface and read/import code

---
+++ pyd3dbsp/import_d3dbsp.py
@@ -0,0 +1,89 @@
+import gc
+import math
+import os
+import struct
+
+import bpy
+import bpy.ops
+import bpy.props
+import bmesh
+
+from . import read_d3dbsp
+
+class D3DBSPImporter(bpy.types.Operator):
+    bl_idname = 'pyd3dbsp.d3dbsp_importer'
+    bl_label = 'CoD2 D3DBSP (.d3dbsp)'
+    bl_options = {'UNDO'}
+
+
+    filepath = bpy.props.StringProperty(subtype = 'FILE_PATH')
+    filename_ext = '.d3dbsp'
+    filter_glob = bpy.props.StringProperty(default="*.d3dbsp", options={'HIDDEN'})
+
+    materialPath = bpy.props.StringProperty(
+        name = 'Material Path',
+        description = 'Directory path containing the materials.',
+        default = '',
+        #subtype = 'DIR_PATH'
+    )
+
+    xmodelPath = bpy.props.StringProperty(
+        name = 'Models Path',
+        description = 'Directory path containing map entities.',
+        default = '',
+        #subtype = 'DIR_PATH'
+    )
+
+    def execute(self, context):
+        #todo
+        result_import_d3dbsp = self.import_d3dbsp(context)
+        #result_import_entities = import_entities()
+
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        bpy.context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+    def import_d3dbsp(self, context):
+        # will need to clean up this mess once i got the whole stuff working
+        d3dbsp = read_d3dbsp.D3DBSP()
+        if(d3dbsp.load_d3dbsp(self.filepath)):
+            
+            for i in range (0, len(d3dbsp.trianglesoups)):
+                mesh = bpy.data.meshes.new('mapgeo_' + str(i) )
+                obj = bpy.data.objects.new('mapgeo_' + str(i), mesh)
+
+                bpy.context.scene.objects.link(obj)
+                bpy.context.scene.objects.active = obj
+                obj.select = True
+
+                mesh = bpy.context.object.data
+                bm = bmesh.new()
+
+                curr_soup = d3dbsp.trianglesoups[i]
+                curr_soup_tricount = (int) (curr_soup.triangle_length / 3)
+                for j in range (0, curr_soup_tricount):
+                    curr_triangle = d3dbsp.triangles[(int) (curr_soup.triangle_offset / 3 + j)]
+
+                    vertex1 = d3dbsp.vertices[(int) (curr_soup.vertex_offset + curr_triangle.v1)]
+                    vertex2 = d3dbsp.vertices[(int) (curr_soup.vertex_offset + curr_triangle.v2)]
+                    vertex3 = d3dbsp.vertices[(int) (curr_soup.vertex_offset + curr_triangle.v3)]
+
+                    v1 = bm.verts.new((vertex1.pos_x, vertex1.pos_y, vertex1.pos_z))
+                    v2 = bm.verts.new((vertex2.pos_x, vertex2.pos_y, vertex2.pos_z))
+                    v3 = bm.verts.new((vertex3.pos_x, vertex3.pos_y, vertex3.pos_z))
+
+                    bm.faces.new((v1, v2, v3))
+                    bm.to_mesh(mesh)
+                
+                bm.free()
+            
+            return True
+        else:
+            return False
+    
+    def import_entities(self, context):
+        pass
+
