825b3963f6e3b3968515357aa09dc1449ed663cd
Soma Rádóczi <mauserzjeh@gmail.com>
2019-05-11 23:28:37 +0200

    Texture decoding

--- pyd3dbsp/read_texture.py
+++ pyd3dbsp/read_texture.py
@@ -5,6 +5,8 @@ import re
 from collections import namedtuple
 from enum import Enum
 
+import texture_decoder as DECODER
+
 TEXTHeader = namedtuple('TEXTHeader', 
     ('magic, version,'
     'format,'
@@ -13,41 +15,55 @@ TEXTHeader = namedtuple('TEXTHeader',
     'filesize,'
     'texture_ofs, mipmap1_ofs, mipmap2_ofs')
     )
-fmt_TEXTHeader = '<3sBccHH2xIIII'
+fmt_TEXTHeader = '<3sBBBHH2xIIII'
 
 class TextureUsage(Enum):
-    Color = b'\x00'
-    Default = b'\x01'
-    Skybox = b'\x05'
+    Color = 0x00
+    Default = 0x01
+    Skybox = 0x05
 
 class TextureFormat(Enum):
-    ARGB32 = b'\x01'
-    RGB24 = b'\x02'
-    GA16 = b'\x03'
-    A8 = b'\x04'
-    DXT1 = b'\x0B'
-    DXT3 = b'\x0C'
-    DXT5 = b'\x0D'
+    ARGB32 = 0x01
+    RGB24 = 0x02
+    GA16 = 0x03
+    A8 = 0x04
+    DXT1 = 0x0B
+    DXT3 = 0x0C
+    DXT5 = 0x0D
 
 class Texture():
 
     def __init__(self):
         self.header = None
-        self.raw_texture_data = []
-        self.raw_mipmap1_data = None
-        self.raw_mipmap2_data = None
+        self.texture_data = None
+        #self.mipmap1_data = None
+        #self.mipmap2_data = None
+
+        self.width = None
+        self.height = None
+        self.format = None
+        self.usage = None
 
     def _read_header(self, file):
         file.seek(0)
         header_data = file.read(struct.calcsize(fmt_TEXTHeader))
         self.header = TEXTHeader._make(struct.unpack(fmt_TEXTHeader, header_data))
         self.header = self.header._replace(magic = self.header.magic.decode('utf-8'))
+        
+        self.width = self.header.width
+        self.height = self.header.height
+        self.format = self.header.format
+        self.usage = self.header.usage
+        
 
     def _read_raw_data(self, file):
         file.seek(self.header.texture_ofs, os.SEEK_SET)
-        for i in range(0, (self.header.filesize - self.header.texture_ofs)):
-            _byte = file.read(1)
-            self.raw_texture_data.append(_byte)
+        raw_data = file.read(self.header.filesize - self.header.texture_ofs)
+        if(self.format == TextureFormat.DXT1.value):
+            self.texture_data = DECODER.decode_dxt1(raw_data, self.width, self.height)
+        elif(self.format and ( TextureFormat.DXT3.value or TextureFormat.DXT5.value )):
+            self.texture_data = DECODER.decode_dxt5(raw_data, self.width, self.height)
+        
 
     def load_texture(self, filepath):
         with open(filepath, 'rb') as file:
